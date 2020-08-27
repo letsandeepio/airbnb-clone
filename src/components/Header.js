acd6c6cb53ba48cd58a14995c8ed5403fc966d5d
letsandeepio <sandeepchopra7@gmail.com>
2020-08-27 15:33:04 -0400

    implements the basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Logo from './Logo';
+import SearchIcon from '@material-ui/icons/Search';
 import './Header.css';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
   return (
     <div className="header">
-      <Logo />
+      <div className="header__logo">
+        <Logo />
+      </div>
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
     </div>
   );
 }
