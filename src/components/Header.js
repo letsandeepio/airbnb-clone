76bb7d65e49a798a8ba5865f934be216627db568
letsandeepio <sandeepchopra7@gmail.com>
2020-08-27 14:56:39 -0400

    project scaffolding

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './Logo';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <Logo />
+    </div>
+  );
+}
+
+export default Header;
