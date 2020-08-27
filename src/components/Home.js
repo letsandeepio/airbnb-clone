5deca534b46f530bac78a38333f4cf4bfb303309
letsandeepio <sandeepchopra7@gmail.com>
2020-08-27 16:50:57 -0400

    adds hero banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Home.css';
+import Banner from './Banner';
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
