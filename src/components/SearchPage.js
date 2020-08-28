57c723796d439df573d4e665e9416dbf5d3c2c1f
letsandeepio <sandeepchopra7@gmail.com>
2020-08-28 09:20:19 -0400

    adds searchPage component

---
+++ src/components/SearchPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+import './SearchPage.css';
+
+function SearchPage() {
+  return (
+    <div className="searchPage">
+      <div className="searchPage__info">
+        <p>62 stays • 26 August to 20 August • 2 guest </p>
+        <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
+      </div>
+    </div>
+  );
+}
+
+export default SearchPage;
