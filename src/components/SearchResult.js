52e3924098e79113b1b040296176230eecd6f565
letsandeepio <sandeepchopra7@gmail.com>
2020-08-28 10:26:15 -0400

    adds searchResult component

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './SearchResult.css';
 
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import StarIcon from '@material-ui/icons/Star';
+
 function SearchResult({
   img,
   location,
   title,
-  desription,
+  description,
   star,
   price,
   total
@@ -13,7 +16,27 @@ function SearchResult({
   return (
     <div className="searchResult">
       <img src={img} alt="" />
-      <FavoriteBorderIcon className="searchResult_heart" />
+      <FavoriteBorderIcon className="searchResult__heart" />
+      <div className="searchResult__info">
+        <div className="searchResult__infoTop">
+          <p>{location}</p>
+          <h3>{title}</h3>
+          <p>____</p>
+          <p>{description}</p>
+        </div>
+        <div className="searchResult__infoBottom">
+          <div className="searchResult__stars">
+            <StarIcon className="searchResult__star" />
+            <p>
+              <strong>{star}</strong>
+            </p>
+          </div>
+        </div>
+        <div className="searchResults_price">
+          <h2>{price}</h2>
+          <p>{total}</p>
+        </div>
+      </div>
     </div>
   );
 }
